61148a4da56e68a972b27279692c175fa248218a
skydreamweb <info@skydream.info>
2020-08-11 17:24:56 +0200

    Camera Page
    Setting up camera page

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,11 +1,31 @@
 <template>
-  <q-page class="flex flex-center">
-   <h2>Camera Page</h2>
+  <q-page class="constrain q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img 
+      class="full-width"
+      src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn size="lg" cround color="grey-10" icon="eva-camera" />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input class="col" label="Caption" dense />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input class="col" label="Location" dense />
+      </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageCamera'
-}
+  name: "PageCamera",
+};
 </script>
+<style lang="scss">
+.camera-frame {
+  border: 2px solid $grey-10;
+  border-radius: 10px;
+}
+</style>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,11 +1,93 @@
 <template>
-  <q-page class="flex flex-center">
-   <h2>Home Page</h2>
+  <q-page class="constrain q-pa-md">
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card class="card-post q-mb-md" flat bordered v-for="(post, index) in posts" :key="index">
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-bold">dalibor</q-item-label>
+              <q-item-label caption>{{ post.location }}</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+          <q-img :src="post.imgUrl" />
+          <q-card-section>
+            <div>{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+
+
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">dalibor</q-item-label>
+            <q-item-label caption>Belgrade, Serbia</q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+
+
+    </div>
   </q-page>
 </template>
 
 <script>
+import { date } from "quasar";
 export default {
-  name: 'PageHome'
-}
+  name: "PageHome",
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: "Golden Gate Bridge",
+          date: 1597151619914,
+          location: "Belgrade, Serbia",
+          imgUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 2,
+          caption: "Golden Gate Bridge",
+          date: 1597151619914,
+          location: "Belgrade, Serbia",
+          imgUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 3,
+          caption: "Golden Gate Bridge",
+          date: 1597151619914,
+          location: "Belgrade, Serbia",
+          imgUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+      ],
+    };
+  },
+  filters: {
+    niceDate(value) {
+      return date.formatDate(value, "D MMMM HH:mm");
+    },
+  },
+};
 </script>
+<style lang="scss" scoped>
+.card-post {
+  .q-img {
+    min-height: 200px;
+  }
+}
+</style>
